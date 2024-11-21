7854d8b9a004a23c6b249f63d8d8a112b442d9c4
Raghav Gaur <raghav@thinkdatalabs.com>
2024-11-21 15:50:27 +0530

    Making New Models

--- main/models.py
+++ main/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Main(models.Model):
 
     name = models.TextField()
+    about = models.TextField()
     def __str__(self):
         return self.name
     # models.IntegerField()
--- main/views.py
+++ main/views.py
@@ -6,11 +6,15 @@ from .models import Main
 
 def home(request):
 
-    return render(request,'front/home.html')
+    sitename="MySite|Home"
+
+    return render(request,'front/home.html', {'sitename':sitename})
 
 def about(request):
+    sitename="MySite|About"
 
-    return render(request,'front/about.html')
+    return render(request,'front/about.html', {'sitename':sitename})
 def contact(request):
+    sitename="MySite|Contact"
 
-    return render(request,'front/contact.html')
+    return render(request,'front/contact.html', {'sitename':sitename})
